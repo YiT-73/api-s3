da74d1fc9320c6a92dc2f2b34698ceed0066f501
YiT <yitzhaknamihas@gmail.com>
2025-07-11 23:28:18 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -1,11 +1,16 @@
 import boto3
+import json
 
 def lambda_handler(event, context):
-    nombre_bucket = event['body']['bucket']
+    body = json.loads(event['body'])  # decodificar el JSON del body
+    nombre_bucket = body['bucket']
+
     s3 = boto3.client('s3')
     s3.create_bucket(Bucket=nombre_bucket)
-    
+
     return {
         'statusCode': 200,
-        'mensaje': f'Bucket {nombre_bucket} creado exitosamente.'
+        'body': json.dumps({
+            'mensaje': f'Bucket {nombre_bucket} creado exitosamente.'
+        })
     }
